b0455626408c5e6d66d55614dfae33a0c346d6f4
Pedro Rodrigues <pedrorodrepo@outlook.com>
2025-06-16 18:48:11 -0300

    BASICS - kotlin equality referential

--- src/main/kotlin/declarations/Declarations.kt
+++ src/main/kotlin/declarations/Declarations.kt
@@ -3,33 +3,68 @@ package declarations
 typealias EmployeeSet = Set<Employee>
 
 fun main(args: Array<String>) {
+
+    val employeeOne = Employee("Pedro", 1)
+    val employeeTwo = Employee("Mary", 2)
+    val employeeThree = Employee("Mary", 2)
+
+    println(employeeOne == employeeTwo)
+    println(employeeTwo == employeeThree)
+    println(employeeOne.equals(employeeTwo))
+    println(employeeTwo.equals(employeeThree))
+    println("-------------------------------------")
+
+    /* check references */
+    println(employeeOne === employeeTwo)
+    println(employeeTwo === employeeThree) // false as java
+
+    val employeeFour = employeeTwo
+    println(employeeFour === employeeTwo)
+    println(employeeFour != employeeTwo)
+    println(employeeTwo != employeeThree)
+    println(employeeTwo !== employeeThree)
+    /*
+    * Java:
+    *     false
+    *     false
+    *     false
+    *     true
+    *
+    */
+
 //    val number: Short = 25
 //    val number = 25
 //    number = 1 error
-
-    var number: Int
-    number = 10
-    number = 20
-
-    val employee1 = Employee("Peter", 500)
-    employee1.name = "Peter Package"
-
-    val employee2: Employee
-    val number2 = 100
-
-    if (number < number2) {
-        employee2 = Employee("Jane", 400)
-    } else {
-        employee2 = Employee("John", 150)
-    }
-
-    println(employee1.name)
-    println(employee2.name)
-
-    val employees: EmployeeSet
-
+//
+//    var number: Int
+//    number = 10
+//    number = 20
+//
+//    val employee1 = Employee("Peter", 500)
+//    employee1.name = "Peter Package"
+//
+//    val employee2: Employee
+//    val number2 = 100
+//
+//    if (number < number2) {
+//        employee2 = Employee("Jane", 400)
+//    } else {
+//        employee2 = Employee("John", 150)
+//    }
+//
+//    println(employee1.name)
+//    println(employee2.name)
+//
+//    val employees: EmployeeSet
 }
 
 class Employee(var name: String, var id: Int) {
 
+    override fun equals(obj: Any?): Boolean {
+        if (obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+
+        return false
+    }
 }
